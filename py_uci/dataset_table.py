58de96e2d0e74e0e9b9f5b3a1c837b0185a1e4b1
Nicki Skafte <skaftenicki@gmail.com>
2019-03-25 12:18:59 +0100

    concrete dataset

--- py_uci/dataset_table.py
+++ py_uci/dataset_table.py
@@ -29,3 +29,4 @@ T.row_append(['boston', 506, 'regression', 'https://archive.ics.uci.edu/ml/machi
 T.row_append(['power_plant', 9568, 'regression', 'https://archive.ics.uci.edu/ml/machine-learning-databases/00294/'])
 T.row_append(['carbon_nanotubes', 10721, 'regression', 'https://archive.ics.uci.edu/ml/machine-learning-databases/00448/'])
 T.row_append(['forest_fire', 517, 'regression', 'https://archive.ics.uci.edu/ml/machine-learning-databases/forest-fires/'])
+T.row_append(['concrete', 1030, 'regression', 'https://archive.ics.uci.edu/ml/machine-learning-databases/concrete/compressive/'])
--- py_uci/datasets/__init__.py
+++ py_uci/datasets/__init__.py
@@ -12,3 +12,4 @@ from .boston import boston
 from .power_plant import power_plant
 from .carbon_nanotubes import carbon_nanotubes
 from .forest_fire import forest_fire
+from .concrete import concrete
